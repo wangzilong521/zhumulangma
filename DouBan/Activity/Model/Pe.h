9cef5330e6a4aeaabe6110c21c868282cd10643c
lanou3g <1337127537@qq.com>
2015-06-28 18:53:52 +0800

    添加了一个类

---
+++ DouBan/Activity/Model/Pe.h
@@ -0,0 +1,13 @@
+//
+//  Pe.h
+//  DouBan
+//
+//  Created by lanou3g on 15/6/28.
+//  Copyright (c) 2015年 蓝鸥科技有限公司. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Pe : NSObject
+@property(nonatomic,copy)NSString *name;
+@end
---
+++ DouBan/Activity/Model/Pe.m
@@ -0,0 +1,13 @@
+//
+//  Pe.m
+//  DouBan
+//
+//  Created by lanou3g on 15/6/28.
+//  Copyright (c) 2015年 蓝鸥科技有限公司. All rights reserved.
+//
+
+#import "Pe.h"
+
+@implementation Pe
+
+@end
